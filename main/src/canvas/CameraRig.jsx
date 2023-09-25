1ea67a0230d61fd70cc788690cafefa4734dd499
praty <pratyushpriyam49@gmail.com>
2023-09-25 20:47:24 +0530

    rotation updated

--- main/src/canvas/CameraRig.jsx
+++ main/src/canvas/CameraRig.jsx
@@ -29,7 +29,7 @@ const CameraRig = ({ children }) => {
     // set the model rotation smoothly
     easing.dampE(
       group.current.rotation,
-      [-state.pointer.y, state.pointer.x, 0],
+      [-state.pointer.y/2, state.pointer.x/2, 0],
       0.25,
       delta
     )
